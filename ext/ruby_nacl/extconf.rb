0bdd79c99436cb03411db1e942d6bb66f91eab4d
Your Name <zac.morris@gmail.com>
2011-10-01 11:38:40 -0700

    Fixing infinite loop in ruby gem

--- ext/ruby_nacl/extconf.rb
+++ ext/ruby_nacl/extconf.rb
@@ -5,14 +5,17 @@ arch = `uname -m`.strip == 'x86_64' ? 'amd64' : 'x86'
 
 HEADER_DIRS = ["NaCl/build/#{hostname}/include/#{arch}"]
 LIB_DIRS = ["NaCl/build/#{hostname}/lib/#{arch}"]
-
 $objs = ["NaCl/build/#{hostname}/lib/#{arch}/randombytes.o", "ruby_nacl.o"]
+
+if !File.exists?(HEADER_DIRS[0]) || !File.exists?(LIB_DIRS[0]) || !File.exists?($objs[0])
+  puts "Building NaCl library before creating Makefile..."
+  `cd NaCl; ./do`
+end
 
 dir_config('ruby_nacl', HEADER_DIRS, LIB_DIRS)
 
-while !find_library('nacl', nil, *LIB_DIRS) || !find_header('crypto_box.h', *HEADER_DIRS) || !File.exists?($objs[0])
-  puts "Building NaCl library before creating Makefile..."
-  `cd nacl; rm -rf build; ./do`
+while !find_library('nacl', nil, *LIB_DIRS) || !find_header('crypto_box.h', *HEADER_DIRS)
+  `cd NaCl; ./do`
 end
 
 create_makefile('ruby_nacl')
